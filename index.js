2e3be8f56c2ee62f90c18632ee304a1b803923f2
Angela <angelakulola4@gmail.com>
2023-03-28 00:04:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Angie Kulola";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`
